accdb17e0662d2a089b5ab16359a1ca9e29f7471
Jay Chavda <jay.chavda@civica2.com>
2025-01-04 00:42:26 +1100

    added storage account creation code

--- main.tf
+++ main.tf
@@ -17,3 +17,17 @@ resource "azurerm_resource_group" "example" {
   name     = "rg1"
   location = "East US"
 }
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "sa1jcazdevops1"  # Storage account name must be unique globally
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    CreatedBy = "Jay"
+    DateCreated = "4-Dec-25"
+    Purpose = "Testing"
+  }
+}
